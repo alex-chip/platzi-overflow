045fd91b9adb592df17cfc22dc825dccee2ac53c
Alex Chip <alex-chip@hotmail.com>
2020-01-09 22:08:17 -0600

    Procesando imagenes, aceptando imagenes

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,12 +1,22 @@
+const { writeFile } = require('fs')
+const { promisify } = require('util')
+const { join } = require('path')
+const uuid = require('uuid/v1')
 const questions = require('../models/index').questions
+
+const write = promisify(writeFile)
 
 async function createQuestion (req, h) {
   if (!req.state.user) {
     return h.redirect('/login')
   }
-  let result
+  let result, filename
   try {
-    result = await questions.create(req.payload, req.state.user)
+    if (Buffer.isBuffer(req.payload.image)) {
+      filename = `${uuid()}.png`
+      await write(join(__dirname, '..', 'public', 'uploads', filename), req.payload.image)
+    }
+    result = await questions.create(req.payload, req.state.user, filename)
     console.log(`Pregunta creada con el  ID: ${result}`)
   } catch (error) {
     console.error(`Ocurrio un error: ${error}`)
@@ -15,7 +25,7 @@ async function createQuestion (req, h) {
       error: 'Problemas creando la pregunta'
     }).takeover()
   }
-  return h.response(`Pregunta creada con el ID ${result}`)
+  return h.redirect(`/question/${result}`)
 }
 
 async function answerQuestion (req, h) {
